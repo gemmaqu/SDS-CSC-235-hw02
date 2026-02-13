d37311f9288fa449f38102210dd11c70b961add0
gemmaqu <gqu@smith.edu>
2026-02-12 22:18:16 -0500

    final

---
+++ index.js
@@ -0,0 +1,7 @@
+function changeColor() {
+    if (document.body.style.backgroundColor === 'lightblue') {
+        document.body.style.backgroundColor = 'white';
+    } else {
+        document.body.style.backgroundColor = 'lightblue';
+    }
+}
